55db9f8deaf316a9f1ebed104a61d3f1c39be1ee
Dmitry Menov <18627661+mdmen@users.noreply.github.com>
2024-05-17 15:47:47 +0600

    Change assets folder structure

--- src/assets/fonts/index.ts
+++
@@ -1,9 +0,0 @@
-import baseFontImage from './base/bitmap.webp';
-import baseFontData from './base/glyphs.json';
-
-export const globalFonts = {
-  base: {
-    url: baseFontImage,
-    data: baseFontData,
-  },
-};
